e42acd29fd78d4b3083e2580b506c790a6953653
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-08 16:12:32 +0500

    completed

--- ex19/Program.cs
+++ ex19/Program.cs
@@ -10,7 +10,48 @@ namespace ex19
     {
         static void Main(string[] args)
         {
+            int limitLine = 10;
+            int limitColumn = 10;
+            int[,] array = new int[limitLine, limitColumn];
+            int maxElement = int.MinValue;
+            Random random = new Random();
 
+            for (int line = 0; line < array.GetLength(0); line++)
+            {
+                for (int column = 0; column < array.GetLength(1); column++)
+                {
+                    array[line, column] = random.Next(int.MinValue, int.MaxValue);
+
+                    if (array[line, column] > maxElement)
+                    {
+                        maxElement = array[line, column];
+                    }
+
+                    Console.Write($"{array[line, column]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int line = 0; line < array.GetLength(0); line++)
+            {
+                for (int column = 0; column < array.GetLength(1); column++)
+                {
+
+                    if (array[line, column] < maxElement)
+                    {
+                        array[line, column] = 0;
+                    }
+
+                    Console.Write($"{array[line, column]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Наибольший элемент равен: {maxElement}");
         }
     }
 }
